401e2fcd7a0591f65c62f26479d5d310c4e1e2f0
Mohamed Nagoor <211001059@rajalakshmi.edu.in>
2024-12-28 20:28:45 +0530

    Added duplicate to check

--- prime_checker.py
+++ prime_checker.py
@@ -8,6 +8,8 @@ def is_prime(n: int) -> bool:
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
+        if n % i == 0:
+            return False
     return True
 
 def find_primes_up_to(limit: int) -> list[int]:
